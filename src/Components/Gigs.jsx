a6c4d5f844942530d788577f293a9969d20aa582
michaeledander1 <michaeledander1@gmail.com>
2018-10-26 22:21:56 +0200

    text centering

--- src/Components/Gigs.jsx
+++ src/Components/Gigs.jsx
@@ -7,12 +7,15 @@ function Gigs() {
   return (
     <div className="gigs-page">
       <div className="gigs-page-title">
-        <Typography>
+        <Typography style={{textAlign: "center", marginTop: "5%"}}>
           <h1>Gigs</h1>
         </Typography>
         <div className="gigs-page-calendar">
           <Calendar />
         </div>
+        <Typography style={{textAlign: "center", marginTop: "3%"}}>
+          <h2>Check back often for updates to the calendar</h2>
+        </Typography>
       </div>
     </div>
   );
